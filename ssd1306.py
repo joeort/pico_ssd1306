8cc86b3e644601c1c94fb5c0c29a5a59a7006dc0
joeort <otto030@gmx.de>
2021-03-04 17:09:31 +0100

    updated README and code

--- ssd1306.py
+++ ssd1306.py
@@ -1,14 +1,26 @@
 import machine
 import utime
+import framebuf
 from ssd1306 import SSD1306_I2C
 
 i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=200000)
 oled = SSD1306_I2C(128, 32, i2c, addr=0x3C)
 
+# Raspberry Pi logo as 32x32 bytearray
+buffer = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+
+# Load the raspberry pi logo into the framebuffer (the image is 32x32)
+fb = framebuf.FrameBuffer(buffer, 32, 46, framebuf.MONO_HLSB)
+
 while True:
-    for i in range(-45,150, 1):
-        oled.fill(0)
-        oled.text('Hello', i, 2)
-        oled.text('World', i, 12)
-        oled.show()
-        utime.sleep_ms(30)
+    # Clear the oled display in case it has junk on it.
+    oled.fill(0)
+
+    # Blit the image from the framebuffer to the oled display
+    oled.blit(fb, 0, 0)
+
+
+    # Finally update the oled display so the image & text is displayed
+    oled.show()
+    utime.sleep(1)
+ 
