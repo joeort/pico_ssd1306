452096c76e2263da32efb562d79dfb63b49ec25a
joeort <otto030@gmx.de>
2021-03-04 16:25:19 +0100

    upload ssd1306.py

---
+++ ssd1306.py
@@ -0,0 +1,14 @@
+import machine
+import utime
+from ssd1306 import SSD1306_I2C
+
+i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=200000)
+oled = SSD1306_I2C(128, 32, i2c, addr=0x3C)
+
+while True:
+    for i in range(-45,150, 1):
+        oled.fill(0)
+        oled.text('Hello', i, 2)
+        oled.text('World', i, 12)
+        oled.show()
+        utime.sleep_ms(30)
